65b94849fed89f2a83b82f64531f7cf1bc8be61c
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-02-21 23:39:29 -0500

    Updates

---
+++ precache-manifest.c7fb9dcef80fabd8df88eaace64c7f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dd0d13a7470d6fe5c33",
+    "url": "/static/css/main.78dda208.chunk.css"
+  },
+  {
+    "revision": "5dd0d13a7470d6fe5c33",
+    "url": "/static/js/main.5dd0d13a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b0a9d09a022d9e4888dd",
+    "url": "/static/js/2.b0a9d09a.chunk.js"
+  },
+  {
+    "revision": "733c8cfdfb237fde5dc78d041c4bbfc8",
+    "url": "/index.html"
+  }
+];
