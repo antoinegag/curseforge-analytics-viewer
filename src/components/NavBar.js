95019746bd868c259ffc0db5d2bb5c8d7786e65b
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-02-22 22:44:11 -0500

    Add source link in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,6 +9,11 @@ export default class NavBar extends React.Component {
     return (
       <Menu style={{ minHeight: "50px"}} inverted attached>
         <Menu.Item><Icon name="pie graph" />CurseForge Analtytics Viewer</Menu.Item>
+        <Menu.Menu position='right'>
+          <Menu.Item href='//github.com/antoinegag/curseforge-analytics-viewer'>
+            <Icon name="github" size="big"/> Source Code
+          </Menu.Item>
+        </Menu.Menu>
       </Menu>
     );
   }
