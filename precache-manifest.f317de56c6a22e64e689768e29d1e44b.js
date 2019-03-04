083b6f65b925d5d865df1827f2d0271441cf216a
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-03-03 21:33:49 -0500

    Updates

---
+++ precache-manifest.f317de56c6a22e64e689768e29d1e44b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a3841853860257dd5c3",
+    "url": "/curseforge-analytics-viewer/static/css/main.e521eb6d.chunk.css"
+  },
+  {
+    "revision": "5a3841853860257dd5c3",
+    "url": "/curseforge-analytics-viewer/static/js/main.5a384185.chunk.js"
+  },
+  {
+    "revision": "a8ace01d15767ad6606d",
+    "url": "/curseforge-analytics-viewer/static/js/runtime~main.a8ace01d.js"
+  },
+  {
+    "revision": "988211e476127e6943af",
+    "url": "/curseforge-analytics-viewer/static/js/2.988211e4.chunk.js"
+  },
+  {
+    "revision": "48eb9d8c90c92612c55a0e3ce07154d3",
+    "url": "/curseforge-analytics-viewer/index.html"
+  }
+];
