44cdeff111befafa20e81e15af25516c9df725da
Antoine Gagnon <antoine.gagnon760@gmail.com>
2020-09-26 21:20:56 -0400

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-600">Test</h1>
+      <Title />
+      <hr className="my-5" />
     </div>
   );
 }
